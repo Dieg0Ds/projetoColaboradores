6099fe1421b7cc7c717a70cf62d97ed0765dfb40
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-04-03 18:16:01 -0300

    updateLucas

--- A.js
+++ A.js
@@ -1,3 +1,6 @@
-let nome = "Diego Silva Santos"
-let cidade = "São Paulo"
-console.log(`Meu nome é ${nome} e moro em ${cidade} capital.`)
+let nome = 'Diego Silva Santos';
+let cidade = 'São Paulo';
+console.log(`Meu nome é ${nome} e moro em ${cidade} capital.`);
+
+let name = 'João Lucas';
+let city = 'Salvador';
