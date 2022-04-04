5a49109ada651e306f27a404df7abf01b4b1419c
Paulo Henrique <ph.blima30@gmail.com>
2022-04-03 22:43:27 -0300

    Adicionando meus dados

--- A.js
+++ A.js
@@ -4,3 +4,9 @@ console.log(`Meu nome é ${nome} e moro em ${cidade} capital.`);
 
 let name = 'João Lucas';
 let city = 'Salvador';
+
+let meuNome = "Paulo Henrique Barbosa de Lima";
+let minhaCidade = "São Paulo";
+console.log(meuNome);
+console.log(minhaCidade);
+
